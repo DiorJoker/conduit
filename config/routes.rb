8b0b894f02c19ceac41c41aa95c48b145b7dd250
poisonmachine <siz.osk.aiti.huku@gmail.com>
2024-01-09 12:10:20 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'static_pages/article'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  #post "/create", to: "articles#create"
+  resources :articles
+
   # Defines the root path route ("/")
   # root "articles#index"
   root 'static_pages#home'
